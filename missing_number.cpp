497488fb1532c12f5940fc766c016eb0f1d218c6
Karan Shetty <shettykaran2110@gmail.com>
2025-08-20 23:18:02 +0530

    Fix vscode config and use g++15

--- missing_number.cpp
+++ missing_number.cpp
@@ -64,6 +64,9 @@ int solve_3(vector<int> &nums) {
     -> xor1 = 1^2^.......^n
     -> xor2 = nums[0]^nums[1]^nums[2]^......^nums[n - 1]
     -> xor1^xor2 is the answer
+
+    Time - O(n)
+    Space - O(1)
 */
 int solve_4(vector<int> &nums) {
     int n = nums.size();
